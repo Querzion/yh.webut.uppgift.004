9efdd44e66ba4553244cebc3a8218338432dd239
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2025-01-20 23:20:18 +0100

    deleted the earlier solution, created a new that was named Presentation, then changed the Solution name from Presentation to Database_assignment, added business class library, and Data class library, created the folder structure that is going to be in use, also added a console application named presentation.console, just in case I would like to expand upon it. will add test projects to for business and data, at a later stage.

--- Database_Assignment/Data/Scripts/CreateTables.sql
+++ Database_Assignment/Data/Scripts/CreateTables.sql
@@ -2,4 +2,3 @@
 -- Created on 20/01/2025 by querzion
 
 -- Write your SQL queries below:
-
--- Database_Assignment/Database_Assignment/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace Database_Assignment.ViewModels;
-
-public abstract class ViewModelBase : ObservableObject
-{
-}
