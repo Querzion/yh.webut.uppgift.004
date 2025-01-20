ddb278b2887a9788c6bc145896b9c771ce05676b
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2025-01-20 23:21:30 +0100

    Delete Database_Assignment directory

--- Database_Assignment/Data/Scripts/CreateTables.sql
+++
@@ -1,4 +0,0 @@
--- SQL Script
--- Created on 20/01/2025 by querzion
-
--- Write your SQL queries below:
--- Database_Assignment/Presentation/ViewModels/MainViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace Presentation.ViewModels;
-
-public partial class MainViewModel : ViewModelBase
-{
-    [ObservableProperty] private string _greeting = "Welcome to Avalonia!";
-}
