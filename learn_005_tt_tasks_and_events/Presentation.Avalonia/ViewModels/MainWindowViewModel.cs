5abf75cb36d568f4400c1d61eade86b21ea2586d
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2025-01-22 18:49:51 +0100

    deleted the tips and trix video course folder.

--- learn_005_tt_tasks_and_events/Presentation.Avalonia/ViewModels/MainWindowViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Presentation.Avalonia.ViewModels;
-
-public partial class MainWindowViewModel : ViewModelBase
-{
-    public string Greeting { get; } = "Welcome to Avalonia!";
-}
--- learn_005_tt_tasks_and_events/Presentation.Avalonia/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace Presentation.Avalonia.ViewModels;
-
-public class ViewModelBase : ObservableObject
-{
-}
